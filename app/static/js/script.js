f92be78ae091f16f10521fd082dd254b80c6802f
ChoiVadim <tsoivadim97@gmail.com>
2024-07-31 03:45:58 +0900

    add styles

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,3 +1,4 @@
+// Change the container based on the button clicked
 function toggleContainer(container) {
     const loginContainer = document.querySelector('.login-container');
     const registerContainer = document.querySelector('.register-container');
@@ -19,12 +20,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const loginBtn = document.getElementById('loginBtn');
     const registerBtn = document.getElementById('registerBtn');
 
+    // Add an event listener to the login button
     loginBtn.addEventListener('click', () => {
         let name = document.getElementById('username_login').value;
         let password = document.getElementById('password_login').value;
         const jsonData = { username: name, password: password };
+        const url = loginBtn.getAttribute('data-url');
 
-        fetch('http://127.0.0.1:5000/login', {
+        fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json; charset=utf-8'
@@ -41,13 +44,14 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch(error => alert('There was a problem with the fetch operation: ' + error.message));
     });
 
-
+    // Add an event listener to the register button
     registerBtn.addEventListener('click', () => {
         let name = document.getElementById('username_register').value;
         let password = document.getElementById('password_register').value;
         const jsonData = { username: name, password: password };
+        const url = registerBtn.getAttribute('data-url');
 
-        fetch('http://127.0.0.1:5000/register', {
+        fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json; charset=utf-8'
